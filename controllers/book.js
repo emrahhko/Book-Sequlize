9ccbffdad224872b6dc5aec17f487bcb058ddd5e
Emrah Hko <emrah.hac@gmail.com>
2024-10-18 20:58:15 +0200

    finish part1

--- controllers/book.js
+++ controllers/book.js
@@ -2,6 +2,34 @@ import db from '../models/index.js';
 
 const Book = db.books;
 
-const bookControllers = {};
+const bookControllers = {
+    getAll : async(req, res) => {
+        const books = await Book.findAll();
+        res.status(200).json(books);
+    },
+    getOne : async(req, res) => {
+        const { id } = req.params;
+        const book = await Book.findOne({id : id});
+        if (book) {
+            res.status(200).json(book);
+        } else {
+            res.status(404).json({ message: `book with id ${id} not found`});
+        }
+    },
+    create : async(req, res) => {
+        const {title, author, price, img, user_id} = req.body;
+        const newBook = {
+            title: title,
+            author: author,
+            price: price,
+            img: img,
+            user_id: user_id
+        };
+        const book = await Book.create(newBook);
+        res.status(201).json(book);
+    },
+    update : async(req, res) => {},
+    remove : async(req, res) => {}
+};
 
 export default bookControllers;
--- utils/hashPassword.js
+++ utils/hashPassword.js
@@ -1,8 +1,21 @@
-import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 
-const hashPassword = (password) => {
-    const salt = bcrypt.genSaltSync(10);
-    return bcrypt.hashSync(password, salt);
-};
+function hashPassword(password) {
+    return crypto
+        .createHash('sha256')
+        .update(password.toString())
+        .digest('base64');
+}
 
 export default hashPassword;
+
+
+
+// import bcrypt from 'bcrypt';
+
+// const hashPassword = (password) => {
+//     const salt = bcrypt.genSaltSync(10);
+//     return bcrypt.hashSync(password, salt);
+// };
+
+// export default hashPassword;
