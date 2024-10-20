ddf782f5bf7cd56156fd1de09ac3bc4aa4df51f1
Emrah Hko <emrah.hac@gmail.com>
2024-10-20 19:03:03 +0200

    finished part2

--- controllers/book.js
+++ controllers/book.js
@@ -3,21 +3,30 @@ import db from '../models/index.js';
 const Book = db.books;
 
 const bookControllers = {
-    getAll : async(req, res) => {
-        const books = await Book.findAll();
-        res.status(200).json(books);
+    getAll: async (req, res) => {
+        try {
+            const books = await Book.findAll();
+            res.status(200).render('books', books);
+        } catch (error) {
+            res.status(500).send({
+                message: 'Some error occurred'
+            });
+        }
     },
-    getOne : async(req, res) => {
+    getOne: async (req, res) => {
         const { id } = req.params;
-        const book = await Book.findOne({id : id});
+        const book = await Book.findOne(where({ id }));
         if (book) {
             res.status(200).json(book);
         } else {
-            res.status(404).json({ message: `book with id ${id} not found`});
+            res.status(404).json({ message: `book with id ${id} not found` });
         }
     },
-    create : async(req, res) => {
-        const {title, author, price, img, user_id} = req.body;
+    getCreateForm: (req, re) => {
+        res.status(200).render('create-book');
+    },
+    create: async (req, res) => {
+        const { title, author, price, img, user_id } = req.body;
         const newBook = {
             title: title,
             author: author,
@@ -28,8 +37,48 @@ const bookControllers = {
         const book = await Book.create(newBook);
         res.status(201).json(book);
     },
-    update : async(req, res) => {},
-    remove : async(req, res) => {}
+    update: async (req, res) => {
+        const { id } = req.params;
+        const { title, author, price, img } = req.body;
+        try {
+            const updatedBook = await Book.update(
+                {
+                    title,
+                    author,
+                    price,
+                    img
+                },
+                where({ id })
+            );
+            res.status(200).send(updatedBook);
+        } catch (err) {
+            res.status(500).send({
+                message: 'Error updating book'
+            });
+        }
+    },
+    remove: async (req, res) => {
+        const { id } = req.params;
+
+        try {
+            const deletedBook = await Book.destroy(where({ id }));
+            res.status(200).send(deletedBook);
+        } catch (err) {
+            res.status(500).send({
+                message: 'Error deleting book with id'
+            });
+        }
+    },
+    getBookByUser: async (req, res) => {
+        const { id } = req.params;
+        try {
+            const book = await Book.findAll(where({ user_id: id }));
+        } catch (err) {
+            res.status(500).send({
+                message: 'Error retrieving book'
+            });
+        }
+    }
 };
 
 export default bookControllers;
